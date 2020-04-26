4cc6096794aaa105d20e997da6ce59d38e056079
Rohit <39048926+rohsin47@users.noreply.github.com>
2020-04-26 21:35:44 +0800

    Add files via upload

--- src/main/java/com/bfm/kafka/consumer/BasicConsumer.java
+++ src/main/java/com/bfm/kafka/consumer/BasicConsumer.java
@@ -1,6 +1,8 @@
 package com.bfm.kafka.consumer;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Properties;
 
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -17,4 +19,11 @@ public class BasicConsumer<K extends Serializable, V extends Serializable> {
 		this.consumer = new KafkaConsumer<K, V>(props);
 	}
 	
+	public void subscribe(String topic) {
+		consumer.subscribe(Collections.singletonList(topic));
+	}
+	
+	public void subscribe(Collection<String> topics) {
+		consumer.subscribe(topics);
+	}
 }
